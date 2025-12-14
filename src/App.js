522b260cfa9e0d820050af8586beddae8dfced03
Bartosz Kordek <bartosz.kordek10@gmail.com>
2025-12-14 11:45:57 +0100

    add title length message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-    let title = 'Wall-E';
-
+    const [title, setTitle] = useState('');
+    
     function handleChange(event) {
-        console.log(event.target.value);
-    } 
+        setTitle(event.target.value);
+    }
+
+    let messsage = '';
+    if (title.length === 0) {
+        messsage = '';
+    } else if(title.length < 5) {
+        messsage = <div>Tytuł jest za krótki. Nagrywają takie filmy?</div>;
+    } else   if(title.length >=5 && title.length <=20) {
+        messsage = <div>Tytuł jest w sam raz, w sam raz na plakat przed kinem.</div>;
+    } else if(title.length > 20) {
+        messsage = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
+    } else {
+        messsage = '';
+    }
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
+            {
+              title.length > 0 && (
+                <div>{messsage}</div>
+              )
+            }
             <input type="text" onChange={handleChange} />
         </div>
     );
