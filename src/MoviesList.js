ae833afe96d7cabb552c6a146915679ef1406f78
Bartosz Kordek <bartosz.kordek10@gmail.com>
2026-01-17 11:47:27 +0100

    add message for empty movies list

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -3,7 +3,7 @@ export default function MovieList(props) {
     return (
         <div >
             <h1>My favourite movies to watch</h1>
-            <h2>Titles</h2>
+            {props.movies.length > 0 ? <h2>Titles</h2> : <p>Brakuje ulubionych filmów</p>}
             <ul>
                 {props.movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
             </ul>
